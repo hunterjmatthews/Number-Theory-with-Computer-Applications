79513a517b50006aa50f1e7bd1bfe47af07eda19
Hunter Matthews <hunter.matthews@utdallas.edu>
2022-10-20 15:32:34 -0500

    Create Exercise 10.py

---
+++ Chapters/Chapter 1/Exercise 10.py
@@ -0,0 +1,18 @@
+"""
+                            Chapter 0: Introduction
+Problem #0.10: Write a computer program to verify Fermat's conjecture
+about the representation of natural numbers as sums of polygonal numbers.
+
+"""
+
+def fermat_polygonal(s, x, n = 0, a = []):
+    y = ~n * (~-n - n * s / 2)
+    if x < y:
+        return (x < 1) * a * (len(a) <= s)
+    else:
+        return fermat_polygonal(s, x, n + 1, a) or fermat_polygonal(s, x - y, n, a + [y])
+
+
+# Encapsulates the program’s primary behavior.
+if __name__ == "__main__":
+    print(fermat_polygonal(6, 43))
